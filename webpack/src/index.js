e29bb33fac2f99d36ac0b36a3444ba97575b7662
caiyuntao <960269915@qq.com>
2020-05-07 17:11:53 +0800

    1

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,11 +1,36 @@
 import _ from 'lodash'; //JavaScript 工具库。
 import txt from '../static/name.txt';
-function component() {
-    var element = document.createElement('div');
-    // element.innerHTML = _.join(['Hello', 'webpack','cc'], '/');
-    element.innerHTML = txt;
+import './style.css' //webpack一切都为模块，可以引入任何，前提是config里面配置了相应的解析loader
+import img from '../static/1.png'; //img会被处理为图片路径
+import printMe from './print.js';
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = _.join(['Hello1', 'webpack'], ' ');
+  element.innerHTML = txt;
+  // 引用外部css
+  element.className = 'reds'
+  // 引用外部js
+  var iocn = new Image();
+  iocn.src = img;
+
+  var btn = document.createElement('button');
+  btn.innerHTML = 'clickme'
+  btn.onclick = printMe;
+
+
+  element.appendChild(btn)
+  element.appendChild(iocn)
+  return element;
+}
+let element = component();
+document.body.appendChild(element);
+
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    document.body.removeChild(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  })
+}
