bb33ee413fb26d38a271871034a47953e4d621dd
caiyuntao <960269915@qq.com>
2021-05-25 18:08:45 +0800

    1

--- node-koa2/wenjianliu.js
+++ node-koa2/wenjianliu.js
@@ -3,33 +3,7 @@ const fs = require("fs");
 
 
 
-// --------------------------读取文件
-let readStream = fs.createReadStream("./wwwroot/txt.txt"); //创建读取流路径
-let str = "";
-let count = 0;
-// 开始读取
-readStream.on("data", (data) => {
-  str += data;
-  count++;
-});
-// 读取结束
-readStream.on("end", () => {
-  console.log(str);
-  console.log(count);
-});
-// 读取出错
-readStream.on("error", (err) => {
-  console.log(err);
-});
 
-// -------------------------写入文件
-let writeStream = fs.createWriteStream('./wwwroot/txt.txt');
-let data = '我是要被写入的数据';
-writeStream.write(data,'UTF8');
-writeStream.end();//标记写入完成
-writeStream.on('finsh',()=>{
-    console.log('写入完成');
-})
 
 
 // ---------------------------复制文件，使用管道流 pipe
