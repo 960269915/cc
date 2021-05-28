45fe17b14f3961c7dd2d2d5204773c684f8572d2
caiyuntao <960269915@qq.com>
2021-05-28 22:15:31 +0800

    1

--- node/expressProject/utils/imgdb.js
+++ node/expressProject/utils/imgdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const conn = mongoose.createConnection("mongodb://127.0.0.1:27017/cc", {
+const conn = mongoose.createConnection("mongodb+srv://ASCII_caiyuntao:caiyuntao@1992@cluster0.n0l7s.mongodb.net/nodedemo?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }) //连接数据库
