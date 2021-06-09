38d6eba996ae033077f14b3e33e4090a362f48c0
caiyuntao <960269915@qq.com>
2021-06-09 17:12:12 +0800

    1

--- node/socket/websocket/server.js
+++ node/socket/websocket/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const websocket = require('ws')
+const websocket = require('ws') //第三方包
 
 
 
@@ -10,17 +10,24 @@ const socket = new websocket.Server({
 
 socket.on("connection", (client) => {
 
-  client.on("open", () => {
-    console.log('socket-start');
+  client.on("open", () => { //client当前链接
+    console.log('服务端开启了socket');
   })
 
   client.on("message", (data) => {
-    socket.clients.forEach((item) => { //所有的链接
+    socket.clients.forEach((item) => { //socket.clients所有的链接
       if (item.readyState === websocket.OPEN) {
+        console.log(`接受客户端的消息${data}`);
         item.send(data)
       }
     })
   })
+
+  client.on("close", () => {
+    console.log('服务端关闭了socket');
+  })
+
+
 })
 // p 65
 
